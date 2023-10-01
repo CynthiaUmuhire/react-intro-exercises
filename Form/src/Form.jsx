2091a6fd8a0ef496f04d8b66328beade2b9a8ef9
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2023-10-01 22:39:06 +0200

    the greet me form

---
+++ Form/src/Form.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+const Form = () => {
+    const [names,setNames] = useState({
+        fName:"",
+        lName:""
+    });
+    const handleFnameChange = (e)=>{
+        setNames((prev) => {
+            return {
+                ...prev,
+                fName:e.target.value
+            }
+        })
+    }
+    const handleLnameChange = (e) => {
+        console.log(e.target.value);
+        setNames((prev) => {
+            return {
+                ...prev,
+                lName:e.target.value
+            }
+        })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(names.fName +" "+names.lName)
+    }
+  return (
+    <form className=" flex flex-col gap-3 items-center justify-center ">
+      <input type="text" value={names.fName} onChange={handleFnameChange} className="p-2 text-left border rounded-lg "/>
+      <input type="text" value={names.lName} onChange={handleLnameChange} className="p-2 text-left border rounded-lg "/>
+      <button className="bg-pink-950 rounded-lg border shadow-xl p-2 text-center text-white" onClick={handleSubmit}> Greet me</button>
+    </form>
+  )
+}
+
+export default Form
