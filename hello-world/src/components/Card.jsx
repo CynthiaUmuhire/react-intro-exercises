02b88489fdfa8cc6ff6e914877a30793c20d9897
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2023-10-01 18:56:49 +0200

    the hello world exercises

---
+++ hello-world/src/components/Card.jsx
@@ -0,0 +1,11 @@
+
+
+const Card = ({text}) => {
+  return (
+    <div className="bg-yellow-600 flex flex-col justify-center items-center h-screen text-black">
+      <p>{text}</p>
+    </div>
+  )
+}
+
+export default Card
