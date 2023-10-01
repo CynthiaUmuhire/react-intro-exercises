54d3346a9d6a41ffc6b84f1439c7e9bfd5785a92
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2023-10-01 19:32:16 +0200

    Capturing events

---
+++ alert-click/src/Button.jsx
@@ -0,0 +1,11 @@
+
+const Button = () => {
+    
+  return (
+      <button className="p-2 border bg-slate-400" onClick={()=>alert("Clicked!")}>
+      <p>Click me</p>
+    </button>
+  )
+}
+
+export default Button
